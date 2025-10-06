9b10642597499ba5f31e3311804cbf7ff7d40f00
Robert Love <robertjohnlove@gmail.com>
2025-10-06 13:49:06 +1100

    docs: Remove Material for MkDocs (#42)

--- docs/hooks.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-
-
-def on_config(config, **kwargs):
-    # Update copyright automatically.
-    config.copyright = f"© {datetime.now().year} Q-CTRL. All rights reserved."
